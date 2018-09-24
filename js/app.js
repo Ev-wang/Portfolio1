c7b4f7487193cc31df2374ff475447dbc7bcaf87
Ev-wang <zwang68@horizon.csueastbay.edu>
2018-09-23 17:54:49 -0700

    Fortfolio Final
    Final portfolio

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-$(document).foundation()
+<!--setinterval-->
+<p id="time1" style="color: blueviolet;"></p>
+<script>
+    function mytime(){
+        var a = new Date();
+        var b = a.toLocaleTimeString();
+        var c = a.toLocaleDateString();
+        document.getElementById("time1").innerHTML = c+"&nbsp"+b;
+        }
+    setInterval(function() {mytime()},1000);
+</script>
+
+---------------------
